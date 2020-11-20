c25ac3e24e87cccde4c569d91eb9217480d2eee2
softwaredamage <teamlads127@gmail.com>
2020-11-20 06:19:28 -0800

    moved js files into scripts folder and css files into style folder - better organization!

--- web/scripts/theme.js
+++ web/scripts/theme.js
@@ -2,10 +2,10 @@ const theme_button = document.querySelector(".theme_button");
 const theme_file = document.querySelector(".theme_file");
 theme_button.addEventListener("click", function () {
     // Swap out the URL for the different stylesheets
-    if (theme_file.getAttribute("href") == "theme_light.css") {
-        theme_file.href = "theme_dark.css";
+    if (theme_file.getAttribute("href") == "styles/theme_light.css") {
+        theme_file.href = "styles/theme_dark.css";
     } else {
-        theme_file.href = "theme_light.css";
+        theme_file.href = "styles/theme_light.css";
     }
 });
 
@@ -13,9 +13,9 @@ const font_button = document.querySelector(".font_button");
 const font_file = document.querySelector(".font_file");
 font_button.addEventListener("click", function () {
     // Swap out the URL for the different stylesheets
-    if (font_file.getAttribute("href") == "font_times.css") {
-        font_file.href = "font_roboto.css";
+    if (font_file.getAttribute("href") == "styles/font_times.css") {
+        font_file.href = "styles/font_roboto.css";
     } else {
-        font_file.href = "font_times.css";
+        font_file.href = "styles/font_times.css";
     }
 });
