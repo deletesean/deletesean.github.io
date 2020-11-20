cded40e0a4397132cf5cf93b2005999bcc410aa1
softwaredamage <teamlads127@gmail.com>
2020-11-20 05:20:58 -0800

    adding a font switch button, also changing user highlight color to red

--- web/theme.js
+++ web/theme.js
@@ -1,10 +1,21 @@
-const btn = document.querySelector(".btn-toggle");
-const theme = document.querySelector("#theme-link");
-btn.addEventListener("click", function () {
+const theme_button = document.querySelector(".theme_button");
+const theme_file = document.querySelector(".theme_file");
+theme_button.addEventListener("click", function () {
     // Swap out the URL for the different stylesheets
-    if (theme.getAttribute("href") == "light.css") {
-        theme.href = "dark.css";
+    if (theme_file.getAttribute("href") == "theme_light.css") {
+        theme_file.href = "theme_dark.css";
     } else {
-        theme.href = "light.css";
+        theme_file.href = "theme_light.css";
+    }
+});
+
+const font_button = document.querySelector(".font_button");
+const font_file = document.querySelector(".font_file");
+font_button.addEventListener("click", function () {
+    // Swap out the URL for the different stylesheets
+    if (font_file.getAttribute("href") == "font_times.css") {
+        font_file.href = "font_roboto.css";
+    } else {
+        font_file.href = "font_times.css";
     }
 });
