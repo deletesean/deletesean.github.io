d0de3e65dc9bc1b56653938411ab43d0fe853fac
softwaredamage <teamlads127@gmail.com>
2020-11-20 04:56:10 -0800

    finally found a way / script to actually pause (or stop) all youtube embeds (iframes) on video html
    yay!

--- web/video_stop.js
+++ web/video_stop.js
@@ -1,12 +1,14 @@
-// Finds all iframes from youtubes and gives them a unique class
-jQuery('iframe[src*="https://www.youtube.com/embed/"]').addClass("youtube-iframe");
+jQuery(".fullscreen").click(function () {
 
-jQuery(".stopVideo").click(function () {
-    // changes the iframe src to prevent playback or stop the video playback in our case
-    $('.youtube-iframe').each(function (index) {
-        $(this).attr('src', $(this).attr('src'));
-        return false;
-    });
+    var stopAllYouTubeVideos = () => {
+        var iframes = document.querySelectorAll('iframe');
+        Array.prototype.forEach.call(iframes, iframe => {
+            iframe.contentWindow.postMessage(JSON.stringify({
+                event: 'command',
+                func: 'pauseVideo'
+            }), '*');
+        });
+    }
+    stopAllYouTubeVideos();
 
-    //click function
 });
